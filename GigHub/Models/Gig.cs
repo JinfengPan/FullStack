a318c59ac71ed12ee3448cb6953f36ed7eb4642d
patrickfly <flypatrickg@163.com>
2016-10-24 23:12:41 +0800

    init

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    /// <summary>
+    /// 现场演唱会
+    /// </summary>
+    public class Gig
+    {
+        public int Id { get; set; }
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
+        public DateTime DateTime { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+    }
+}
---
+++ GigHub/Models/GigFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class GigFormViewModel
+    {
+        [Required]
+        public string Venue { get; set; }
+        [Required]
+        public string Date { get; set; }
+        [Required]
+        public string Time { get; set; }
+        [Required]
+        public byte Genre { get; set; }
+
+        public List<Genre> Genres { get; set; }
+
+        public DateTime DateTime => DateTime.Parse($"{Date}{Time}");
+    }
+}
