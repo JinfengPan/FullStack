33bcd7f18a09c3fb1415dd1e8e633d2e0de1ee0b
Patrick Pan <jinfeng.pan@31huiyi.com>
2016-10-26 13:00:03 +0800

    d

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -5,8 +5,15 @@ namespace GigHub.Models
 {
     // 可以通过向 ApplicationUser 类添加更多属性来为用户添加配置文件数据。若要了解详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=317594。
 
+
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Attendance> Attendances
+        {
+            get;
+            set;
+        }
         public DbSet<Gig> Gigs { get; set; }
 
         public DbSet<Genre> Genres { get; set; }
@@ -19,6 +26,13 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>().HasRequired(a => a.Gig)
+                .WithMany().WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
